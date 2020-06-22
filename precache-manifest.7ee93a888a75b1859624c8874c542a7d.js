cdefe488fa091eedd62e024081618c6b7f04a067
Zakky Muhammad Fajar <zakkymf@gmail.com>
2020-06-22 18:03:24 +0700

    Updates

---
+++ precache-manifest.7ee93a888a75b1859624c8874c542a7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5f8bcbaca36d19f21976714365f360",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "2148a01592ddb407bced",
+    "url": "/portofolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/portofolio/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portofolio/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2148a01592ddb407bced",
+    "url": "/portofolio/static/js/main.84462d64.chunk.js"
+  },
+  {
+    "revision": "ef951ed90a186183cea3",
+    "url": "/portofolio/static/js/runtime-main.002dd87e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portofolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
